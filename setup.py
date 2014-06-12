10de74c14d45dc896b967a716b5fa08568821964
Thomas Levine <_@thomaslevine.com>
2014-06-11 20:40:48 -0400

    version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='tlevine',
           'lxml',
       ],
       tests_require = ['nose'],
-      version='0.0.13',
+      version='0.0.14',
       license='AGPL',
       classifiers=[
           'Programming Language :: Python :: 2.7',

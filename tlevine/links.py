b9b89260f18adf51891bb0d490029cc524c33fe3
Thomas Levine <_@thomaslevine.com>
2014-06-11 20:40:33 -0400

    catch errors a bit better

--- tlevine/links.py
+++ tlevine/links.py
@@ -29,18 +29,35 @@ if use_cache:
     @cache(os.path.join(cachedir,'pypi'))
     def pypi_packages():
         client = xmlrpc_client.ServerProxy('http://pypi.python.org/pypi')
-        return client.user_packages('tlevine')
+        try:
+            return client.user_packages('tlevine')
+        except Exception as e:
+            sys.stderr.write('Error from PyPI:\n%s' % e)
+            raise
 else:
-    get = requests.get
+    def get(url):
+        try:
+            return requests.get(url)
+        except Exception as e:
+            sys.stderr.write('Error at %s:\n%s' % (url, e))
+            raise
     def pypi_packages():
-        return xmlrpc_client.ServerProxy('http://pypi.python.org/pypi').user_packages('tlevine')
+        try:
+            return xmlrpc_client.ServerProxy('http://pypi.python.org/pypi').user_packages('tlevine')
+        except Exception as e:
+            sys.stderr.write('Error from PyPI:\n%s' % e)
+            raise
 
 def github(username):
     def pages():
         for page_number in itertools.count(1):
             url = 'https://api.github.com/users/%s/repos?page=%d' % (username, page_number)
-            response = get(url)
-            yield json.loads(response.text)
+            try:
+                response = get(url)
+            except:
+                pass
+            else:
+                yield json.loads(response.text)
 
     for page in itertools.takewhile(lambda r: r != [], pages()):
         for repository in page:
@@ -48,37 +65,53 @@ def github(username):
 
 def thomaslevine():
     url = 'http://thomaslevine.com/!/'
-    response = get(url)
-    html = lxml.html.fromstring(response.text)
-    html.make_links_absolute(url)
-    return (unicode(link) for link in html.xpath('//a/@href') if link.startswith(url))
+    try:
+        response = get(url)
+    except:
+        return []
+    else:
+        html = lxml.html.fromstring(response.text)
+        html.make_links_absolute(url)
+        return (unicode(link) for link in html.xpath('//a/@href') if link.startswith(url))
 
 def scraperwiki(url = 'https://classic.scraperwiki.com/profiles/tlevine/index.html'):
-    response = get(url)
-    html = lxml.html.fromstring(response.text)
-    html.make_links_absolute(url)
-    for href in html.xpath('//li[@class="code_object_line"]/descendant::h3/a[position()=2]/@href'):
-        yield re.sub(r'index.html$', '', unicode(href))
-    nexts = html.xpath(u'//a[text()="Next »"]/@href')
-    if nexts != []:
-        for scraper in scraperwiki(nexts[0]):
-            yield scraper
+    try:
+        response = get(url)
+    except:
+        pass
+    else:
+        html = lxml.html.fromstring(response.text)
+        html.make_links_absolute(url)
+        for href in html.xpath('//li[@class="code_object_line"]/descendant::h3/a[position()=2]/@href'):
+            yield re.sub(r'index.html$', '', unicode(href))
+        nexts = html.xpath(u'//a[text()="Next »"]/@href')
+        if nexts != []:
+            for scraper in scraperwiki(nexts[0]):
+                yield scraper
 
 def gitorious():
     url = 'https://gitorious.org/tlevine.xml'
-    response = get(url)
-    project = lxml.html.fromstring(response.text.encode('utf-8'))
-    project.make_links_absolute(url)
-    for repository in project.xpath('//repository[owner[text()="tlevine"]]'):
-        yield 'https' + repository.xpath('clone_url/text()')[0][3:-4]
-
+    try:
+        response = get(url)
+    except:
+        pass
+    else:
+        project = lxml.html.fromstring(response.text.encode('utf-8'))
+        project.make_links_absolute(url)
+        for repository in project.xpath('//repository[owner[text()="tlevine"]]'):
+            yield 'https' + repository.xpath('clone_url/text()')[0][3:-4]
+ 
 def npm():
     url = 'https://www.npmjs.org/~tlevine'
-    response = get(url)
-    html = lxml.html.fromstring(response.text)
-    html.make_links_absolute(url)
-    return map(unicode, html.xpath('id("profile")/ul/li/a/@href'))
-
+    try:
+        response = get(url)
+    except:
+        return []
+    else:
+        html = lxml.html.fromstring(response.text)
+        html.make_links_absolute(url)
+        return map(unicode, html.xpath('id("profile")/ul/li/a/@href'))
+ 
 def pypi():
     for role, package in pypi_packages():
         yield 'https://pypi.python.org/pypi/%s' % package
